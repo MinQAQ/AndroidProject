f2e9044b1e018bf4a6efc587a1b40948f50b1017
MinQaQ <107910965@qq.com>
2022-11-10 17:15:04 +0800

    up

--- Music/app/src/main/java/com/bugdeve/music/MainActivity.kt
+++ Music/app/src/main/java/com/bugdeve/music/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.bugdeve.music
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bugdeve.music.ui.theme.MusicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MusicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "Login", ){
+                composable("Login"){
+                    Login(navController)
+                }
+                composable("App"){
+                    App(navController)
                 }
             }
         }
@@ -30,14 +37,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Login(navController: NavController){
+    var username by remember (){
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(value = username, onValueChange = {username=it})
+
+        Button(onClick = {
+            navController.navigate("App")
+        }) {
+            Text(text = "登陆")
+        }
+    }
+}
+@Composable
+fun App(navController: NavController){
+    Column(
+        modifier = Modifier.fillMaxSize(), 
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(text = "App")
+    }
+    
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MusicTheme {
-        Greeting("Android")
+
     }
 }
